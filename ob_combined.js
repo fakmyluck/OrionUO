96232968dbb9f7f9f882c05d65cd184fa6a64ec9
fuckmyluck <akozuhvoski@gmail.com>
2023-12-07 15:56:26 +0200

    hz che

--- ob_combined.js
+++ ob_combined.js
@@ -142,11 +142,11 @@ function add_threats(mobs){
                 newmob_found=false
 
         }
-        if(newmob_found){   Line 145 Result of expression thrers_arr[threats_arr,length] [undefined]
-            threats_arr[threats_arr.length][0]=mobs[m].Name()
-            threats_arr[threats_arr.length][1]=mobs[m].Serial()
-            threats_arr[threats_arr.length][2]=mobs[m].Exists()
-            threats_arr[threats_arr.length][3]=new Date()
+        if(newmob_found){  // Line 145 Result of expression thrers_arr[threats_arr,length] [undefined]
+            threats_arr[threats_arr.length]=mobs[m].Name()
+            threats_arr[threats_arr.length]=mobs[m].Serial()
+            threats_arr[threats_arr.length]=mobs[m].Exists()
+            threats_arr[threats_arr.length]=new Date()
         };
             // {
             //     Name:mobs[m].Name(),
